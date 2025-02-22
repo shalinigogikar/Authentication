d3406de97628661e5fe7243fddd4df6dc5dddc59
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-22 23:43:30 +0530

    sign in is added

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -3,26 +3,60 @@ import { useState, useRef } from 'react';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef=useRef();
+  const passwordInputRef=useRef();
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
+  const submitHandler=(event)=>{
+    event.preventDefault();
+    const enteredEmail=emailInputRef.current.value;
+    const enteredPassword=passwordInputRef.current.value;
+  if(isLogin){
 
+  }else{
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAMmgg2jojdiep3FIe6nkIf_3aS6B-xQgo',
+    {
+      method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    email:enteredEmail,
+    password: enteredPassword,
+    returnSecureToken: true
+  })
+  })
+  .then((res)=>{
+  if(res.ok){
+    return res.json();
+  }else{
+    return res.json().then((data)=>{
+      console.log(data);
+    });
+  }
+  })
+   .catch((err) => {
+    console.error('Error:', err);
+  });
+}
+  };
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} autoComplete="email" />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
           <input
             type='password'
             id='password'
-            required
+            required ref={passwordInputRef} autoComplete="new-password"
           />
         </div>
         <div className={classes.actions}>
