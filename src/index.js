cabc63d87b4a066b43f52f41790c92a75b3fe602
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-24 00:43:51 +0530

    wrapped with context

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { AuthContextProvider } from './components/Store/AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <AuthContextProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
