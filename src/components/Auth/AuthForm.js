fffb41e62aa8af5b70f81eae67b54f5249f4a336
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-23 22:00:56 +0530

    msg will be displayed when signup is successful or failed

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -4,14 +4,24 @@ import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [msg,setMsg]=useState('');
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const [isError,setIsError]=useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
+    setMsg('');
+    setIsError(false);
   };
-
-<<<<<<< HEAD
+const submitHandler=(event)=>{
+  event.preventDefault();
+  const enteredEmail=emailInputRef.current.value;
+  const enteredPassword=passwordInputRef.current.value;
+if(isLogin){
+  console.log("Logging in:", enteredEmail);
   }else{
-    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={myAPIKey}',
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAMmgg2jojdiep3FIe6nkIf_3aS6B-xQgo',
     {
       method: 'POST',
   headers: {
@@ -23,39 +33,41 @@ const AuthForm = () => {
     returnSecureToken: true
   })
   })
-  .then((res)=>{
-  if(res.ok){
-    return res.json();
-  }else{
-    return res.json().then((data)=>{
-      console.log(data);
-    });
+  .then(async(res)=>{
+    const data=await res.json();
+  if(!res.ok){
+    throw new Error(data.error?.message || 'Signup failed');
   }
-  })
+  setMsg('Signup successful!');
+   setIsError(false);
+      console.log(data);
+    })
    .catch((err) => {
     console.error('Error:', err);
+    setMsg("signUp Failed",err.Msg);
+    setIsError(true);
   });
 }
   };
-=======
->>>>>>> parent of d3406de (sign in is added)
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
           <input
             type='password'
             id='password'
-            required
+            required ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
+        <button type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
           <button
             type='button'
             className={classes.toggle}
@@ -65,6 +77,7 @@ const AuthForm = () => {
           </button>
         </div>
       </form>
+      {msg && <p>{msg}</p>}
     </section>
   );
 };
