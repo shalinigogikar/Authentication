d13c01cf6ac97999454d8fa6274b1683e5288089
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-02-24 18:59:14 +0530

    Autologout implemented with useEffect

--- src/components/Store/AuthContext.js
+++ src/components/Store/AuthContext.js
@@ -1,4 +1,4 @@
-import React,{ useState} from "react";
+import React,{ useState,useEffect} from "react";
 const AuthContext=React.createContext({
 token:'',
 isLoggedIn:false,
@@ -6,16 +6,33 @@ login:(token)=>{},
 logout:()=>{},
 });
 export const AuthContextProvider=(props)=>{
-    const initialToken=localStorage.getItem('token');
-    const [token,setToken]=useState(initialToken);
+    const storedToken=localStorage.getItem('token');
+    const storedExpiration=localStorage.getItem('expirationTime');
+    const [token,setToken]=useState(storedToken);
     const userIsLoggedIn=!!token;
+    useEffect(()=>{
+        if(storedToken&&storedExpiration)
+        {
+            const remainingTime=new Date(storedExpiration)-new Date();
+            if(remainingTime<=0)
+            {
+                logoutHandler();
+            }else{
+                setTimeout(logoutHandler,remainingTime);
+            }
+        }
+    },[storedToken,storedExpiration]);
     const loginHandler=(token)=>{
-        setToken(token);
+        const expirationTime=new Date(new Date().getTime()+5*60*1000);
         localStorage.setItem('token',token);  
+        localStorage.setItem('expirationTime',expirationTime);   
+        setToken(token);
+        setTimeout(logoutHandler,5*60*1000);
       };
     const logoutHandler=()=>{
         setToken(null);
         localStorage.removeItem('token');
+        localStorage.removeItem('expirationTime');
     };
     const contextValue={
         token:token,
